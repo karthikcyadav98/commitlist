acde2808c7b54178c6e2495df79c22fd82b17f62
karthikcyadav98 <karthikcy98@gmail.com>
2020-11-20 19:47:06 +0530

    getting data from github public api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import CommitList from './pages/CommitList';
-import axios from 'axios';
-import {get_commit_list} from './apis/apis';
 
 const Header = () => {
-	useEffect(() => {
-		axios({
-			method: 'GET',
-			url: get_commit_list
-		})
-			.then(response => {
-				console.log('ajhdad', response.data);
-			})
-			.catch(e => console.error(e));
-	}, []);
-
 	return (
 		<ErrorBoundary>
 			<CommitList />
--- src/pages/CommitList.js
+++ src/pages/CommitList.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {get_commit_list} from '../apis/apis';
+import CommitItem from './CommitItem';
 
-const CommitList = () => {
-	return (
-		<div>
-			<p>CommitList</p>
-		</div>
-	);
+const CommitItems = () => {
+	const [commitList, setCommitList] = useState([]);
+
+	useEffect(() => {
+		axios({
+			method: 'GET',
+			url: get_commit_list
+		})
+			.then(response => {
+				setCommitList(response.data);
+			})
+			.catch(e => console.error(e));
+	}, []);
+
+	return <div>{commitList.map((item, index) => <CommitItem key={index} />)}</div>;
 };
 
-export default CommitList;
+export default CommitItems;
