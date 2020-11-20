d62792d9b5a3e6f37f915a496860f2825b547395
karthikcyadav98 <karthikcy98@gmail.com>
2020-11-20 20:42:37 +0530

    header is addedto the website

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -1,1 +1,4 @@
-export const get_commit_list = 'https://api.github.com/repos/karthikcyadav98/commitlist/commits';
+export const username = 'karthikcyadav98';
+export const repo_name = 'commitlist';
+
+export const get_commit_list = `https://api.github.com/repos/${username}/${repo_name}/commits`;
--- src/common/Header.js
+++ src/common/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {repo_name, username} from '../apis/apis';
+import {Grid} from 'semantic-ui-react';
 
 const Header = () => {
 	return (
-		<div style={{textAlign: 'center'}}>
-			<p>Header</p>
-		</div>
+		<Grid container style={{textAlign: 'left', marginTop: 10}}>
+			<Grid.Column width={16}>
+				<p style={{color: '#0366D6', fontSize: 30}}>
+					{username} / <b>{repo_name}</b>
+				</p>
+			</Grid.Column>
+		</Grid>
 	);
 };
 
